e6627545cb0867ef525fea059144f93926a4f5c3
Yana Anatska <69202027+rozhnovaYana@users.noreply.github.com>
2024-03-28 17:50:23 +0200

    Added provider

--- src/app/Provider.tsx
+++ src/app/Provider.tsx
@@ -1,9 +1,14 @@
 "use client";
 import { NextUIProvider } from "@nextui-org/react";
+import { SessionProvider } from "next-auth/react";
 import React, { ReactNode } from "react";
 
 const Provider = ({ children }: { children: ReactNode }) => {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return (
+    <SessionProvider>
+      <NextUIProvider>{children}</NextUIProvider>
+    </SessionProvider>
+  );
 };
 
 export default Provider;
